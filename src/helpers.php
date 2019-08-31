76a064c823f5b2b7f9bf1d459655e0602f08835e
Machiel <CheatCodes@users.noreply.github.com>
2019-08-31 10:21:11 +0200

    Add function_exists and docblock to helper function

--- src/helpers.php
+++ src/helpers.php
@@ -3,14 +3,19 @@
 use Divtag\LaravelEtag\Etag;
 use Divtag\LaravelEtag\Facade;
 
-function entity_tag()
-{
-    /** @var Etag $etag */
-    $etag = Facade::getFacadeRoot();
+if (!function_exists('entity_tag')) {
+    /**
+     * @return Etag
+     */
+    function entity_tag()
+    {
+        /** @var Etag $etag */
+        $etag = Facade::getFacadeRoot();
 
-    if (func_num_args()) {
-        $etag->update(...func_get_args());
+        if (func_num_args()) {
+            $etag->update(...func_get_args());
+        }
+
+        return $etag;
     }
-
-    return $etag;
 }
